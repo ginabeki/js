c0e96042b7c05f770ae0c103dda9cf4640dddf13
ginabeki <ginabeki3@gmail.com>
2022-02-28 14:44:39 +0300

    type conversion

---
+++ javascript-tutorials/type-conversion/index.js
@@ -0,0 +1,13 @@
+//Type Conversion of numbers, strings, booleans
+//(Explicit vs Implicit)
+
+//Change the datatype of a value to another
+
+var myVar;
+
+//myVar = Number ("3.14");
+//myVar = String (3.14);
+//myVar = Boolean ("False");
+
+console.log(typeof myVar);
+console.log(myVar);
