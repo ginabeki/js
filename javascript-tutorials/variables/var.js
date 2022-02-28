6ae5f107dd37b0244fb7634d222ebceda18dbea8
ginabeki <ginabeki3@gmail.com>
2022-02-28 13:36:53 +0300

    variables

---
+++ javascript-tutorials/variables/var.js
@@ -0,0 +1,23 @@
+var age = 21;
+//To assign a string you must place quotes
+var firstName = "Bro";
+var online = true;
+// var whatever;
+// whatever = "Code";
+var whatever = null;
+
+age = age + 1;
+var fullName = firstName + " " + lastName;
+
+//limited to the scope of a block sta stack
+let mySweetVariable = 420;
+
+// const = constant reference to val
+const pi = 3.14159;
+
+//To display age with a string we use + or comma
+console.log("Your age is", age);
+console.log("Hello", fullName);
+console.log("Are you online?:", online);
+//When you declare a variable without any value assigned, it will display undefined
+console.log(whatever);
