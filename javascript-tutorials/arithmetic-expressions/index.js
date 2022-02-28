0f71d8675628e9b17696cd8f6ef9af73c3c90b28
ginabeki <ginabeki3@gmail.com>
2022-02-28 14:04:11 +0300

    Basic arithmetic js expressions

---
+++ javascript-tutorials/arithmetic-expressions/index.js
@@ -0,0 +1,30 @@
+/*
+airthmetc expression is some combination
+of operands (values, variables, etc.)
+operators (+ - * / %)
+that can be evaluated to a value
+*/
+
+var friends = 10;
+
+// friends = friends + 1;
+// friends = friends - 1;
+//friends = friends * 2;
+//friends = friends / 2;
+//friends = friends ** 2;
+
+// var remainingFriends = friends % 3;
+
+//SHORTCUT IF YOU'RE REASSIGNING THE SAME VARIABLE
+//friends += 1;
+//friends -= 1;
+//friends *= 2;
+//friends /= 2;
+//friends **=2;
+//friends %=3;
+
+// console.log(remainingFriends);
+
+// operator precedence (Bodmas)
+friends = 10 + 2 / 2;
+console.log(friends);
