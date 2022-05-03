928224f22c7ff471ead3db9e6d6c4bec4e9c9401
ginabeki <ginabeki3@gmail.com>
2022-05-03 08:04:59 +0300

    update

---
+++ variables/variables.js
@@ -0,0 +1,25 @@
+var name = "Gina"; // Datatype string
+var id = 12345; // Datatype number
+var alive = true; //Boolean
+var fiveDs; // undefined
+
+name = "Ricky";
+name = name + " Beki";
+console.log(name);
+
+// id = 7 + 3;
+// id = id - 2;
+// id = id * 4
+// id = id /4
+// console.log(id);
+
+// Complex datatypes
+
+fiveDs = ["Dodge", "Duck", "Dip", "Dive", "Dodge"];
+// Array (list)
+console.log(fiveDs[0] + " " + fiveDs[2]);
+
+// object
+//function
+//date
+//DOM object - Document object model
