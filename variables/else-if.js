6803234ddf8c208abb00ea2bfb5f7b216c6a9745
ginabeki <ginabeki3@gmail.com>
2022-05-03 08:25:41 +0300

    EsleIf

---
+++ variables/else-if.js
@@ -0,0 +1,21 @@
+let first_name = "Gina";
+let last_name = "Bobby";
+let age = 45;
+let first = true;
+let last = false;
+let bob; // undefined cos nothing is declared
+
+if (age <= 45) {
+  // if the value of age is less than or equal to 40
+} else if (age > 40 && age < 50) {
+  // if age is greater than 40 and less than 50
+  // && - means AND
+  // || - means OR
+} else if (age < 0) {
+  // otherwise prevent something from hapening
+} else {
+  //greater than 50
+}
+
+// Comparison operators
+// < <= > >= == != ===
---
+++ variables/if-else.js
@@ -0,0 +1,19 @@
+let first_name = "Gina";
+let last_name = "Bobby";
+let age = 45;
+let first = true;
+let last = false;
+let bob; // undefined cos nothing is declared
+
+// Falsey
+// false, null, undefined, 0, "", '', NaN
+
+// if(last){
+
+// }
+
+if (age == 45) {
+  // do something
+} else {
+  // do something if false
+}
